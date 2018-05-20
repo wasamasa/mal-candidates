8f335a81318a091032ca88ee138281eb90aa5086
Vasilij Schneidermann <mail@vasilij.de>
2018-05-20 14:58:59 +0200

    Remove unnecessary docker-run args

--- lush/docker-run.sh
+++ lush/docker-run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 GIT_TOP=$(git rev-parse --show-toplevel)
-docker run -e COLUMNS="`tput cols`" \
-       -e LINES="`tput lines`" \
-       -it --rm \
+docker run -it --rm \
        --volume=${GIT_TOP}/lush:/mal \
        --volume=${GIT_TOP}/lush/.lush:/root/.lush \
        --volume=${GIT_TOP}/lush/C:/usr/share/lush/lsh/libc/C \
